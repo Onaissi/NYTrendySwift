f19525f9e9d5100be93fa6a6d2c8285adfafb802
AOnaissi <22060999>
2019-06-17 14:54:28 +0300

    Initial Commit

---
+++ NYTrendy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTrendy
+//
+//  Created by Mac on 6/17/19.
+//  Copyright © 2019 onaissi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
